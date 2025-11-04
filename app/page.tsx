b145ba86effe608b318d3d85f37bff207b712063
GovDraft <govdraft@gmail.com>
2025-11-04 00:54:10 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import createBrowserClient from '../../lib/supabaseClient'
+import createBrowserClient from '../lib/supabaseClient';
 
 export default function LoginPage() {
   const supabase = createBrowserClient()
