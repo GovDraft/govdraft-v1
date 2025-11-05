2725c902fa8a9dc00270946e729c1873d838a35f
GovDraft <govdraft@gmail.com>
2025-11-05 01:58:37 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,7 @@
 // lib/supabaseClient.ts
-'use client';
-
 import { createBrowserClient } from '@supabase/ssr';
 
-export default function createBrowserClient() {
+export default function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
