0f765f366c5f7eede9ffa3b03667c26fb066339f
GovDraft <govdraft@gmail.com>
2025-11-05 22:31:42 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,18 +3,9 @@
 
 import { createBrowserClient } from '@supabase/ssr';
 
-export default function createSupabaseBrowser() {
+export default function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      // 👇 Keep the browser in implicit flow, too (we’re using magic links)
-      auth: {
-        flowType: 'implicit',
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: true,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
